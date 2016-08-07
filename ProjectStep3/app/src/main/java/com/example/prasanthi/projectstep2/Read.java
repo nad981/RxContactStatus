409f53ab09d26e0f27a9f33f24373f6468a2af75
nad981 <nad981@my.utsa.edu>
2016-08-06 19:05:47 -0500

    Everything

---
+++ ProjectStep3/app/src/main/java/com/example/prasanthi/projectstep2/Read.java
@@ -0,0 +1,15 @@
+package com.example.prasanthi.projectstep2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Prasanthi on 7/27/2016.
+ */
+public class Read extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read);
+    }
+}
