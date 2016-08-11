826ac0a75df72b821f254f7009aeb6aa806b5874
nad981 <nad981@my.utsa.edu>
2016-08-10 23:58:31 -0500

    step2

---
+++ ContactsTest/app/src/main/java/com/example/prasanthi/contactstest/DBhelper.java
@@ -0,0 +1,42 @@
+package com.example.prasanthi.contactstest;
+
+/**
+ * Created by Prasanthi on 7/5/2016.
+ */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBhelper extends SQLiteOpenHelper {
+
+    // TABLE INFORMATTION
+    public static final String TABLE_MEMBER = "member";
+    public static final String MEMBER_ID = "_id";
+    public static final String MEMBER_NAME = "name";
+
+    // DATABASE INFORMATION
+    static final String DB_NAME = "MEMBER.DB";
+    static final int DB_VERSION = 1;
+
+    // TABLE CREATION STATEMENT
+    private static final String CREATE_TABLE = "create table "
+            + TABLE_MEMBER + "(" + MEMBER_ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + MEMBER_NAME + " TEXT NOT NULL);";
+
+    public DBhelper(Context context) {
+        super(context, DB_NAME, null,DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MEMBER);
+        onCreate(db);
+    }
+}
+
---
+++ ContactsTest/app/src/main/java/com/example/prasanthi/contactstest/Modify_member.java
@@ -0,0 +1,71 @@
+package com.example.prasanthi.contactstest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Modify_member extends Activity implements OnClickListener {
+
+    EditText et;
+    Button edit_bt, delete_bt;
+
+    long member_id;
+
+    SQLController dbcon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.modify_member);
+
+        dbcon = new SQLController(this);
+        dbcon.open();
+
+        et = (EditText) findViewById(R.id.edit_mem_id);
+        edit_bt = (Button) findViewById(R.id.update_bt_id);
+        delete_bt = (Button) findViewById(R.id.delete_bt_id);
+
+        Intent i = getIntent();
+        String memberID = i.getStringExtra("memberID");
+        String memberName = i.getStringExtra("memberName");
+
+        member_id = Long.parseLong(memberID);
+
+        et.setText(memberName);
+
+        edit_bt.setOnClickListener(this);
+        delete_bt.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.update_bt_id:
+                String memName_upd = et.getText().toString();
+                dbcon.updateData(member_id, memName_upd);
+                this.returnHome();
+                break;
+
+            case R.id.delete_bt_id:
+                dbcon.deleteData(member_id);
+                this.returnHome();
+                break;
+        }
+    }
+
+    public void returnHome() {
+
+        Intent home_intent = new Intent(getApplicationContext(),
+                MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        startActivity(home_intent);
+    }
+
+}
